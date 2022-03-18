d70fb47cfc3a70ace0ae4d2c8597dd5a06994035
Michael Aufreiter <michael@letsken.com>
2022-03-18 12:44:40 +0100

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -28,19 +28,17 @@
 
 	<!-- TODO lose the @next! -->
 	<pre>npm init svelte@next</pre>
-
 	<p>
 		The page you're looking at is purely static HTML, with no client-side interactivity needed.
 		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
 		the devtools network panel and reloading.
 	</p>
-
 	<p>
 		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
 		it with JavaScript disabled!
 	</p>
 	<p>
-		And here I add another paragraph.
+		Change the text of another paragraph.
 	</p>
 </div>
 
